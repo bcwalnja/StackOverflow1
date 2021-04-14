219eadfa55809aff81568da35852be11ddc29cc3
Nate Waldron <bcwalnja@gmail.com>
2021-04-14 14:51:04 -0400

    added loops

--- Program.cs
+++ Program.cs
@@ -33,13 +33,15 @@ namespace StackOverflow1
                 int.TryParse(input, out second);
             }
 
-            if (second % first == 0)
+            var remainder = second % first;
+
+            if (remainder == 0)
             {
                 Console.WriteLine("It's an even division! There's no remainder.");
             }
             else
             {
-                Console.WriteLine($"There was a remainder left over. {second % first}");
+                Console.WriteLine($"There was a remainder left over. {remainder}");
             }
 
             var division = (double)second / (double)first;
@@ -48,10 +50,39 @@ namespace StackOverflow1
             var subtraction = (double)second - (double)first;
 
 
-            Console.WriteLine($"The {nameof(division)} of the numbers is: {division:N2}");
-            Console.WriteLine($"The {nameof(multiplication)} of the numbers is: {multiplication:N2}");
-            Console.WriteLine($"The {nameof(addition)} of the numbers is: {addition:N2}");
-            Console.WriteLine($"The {nameof(subtraction)} of the numbers is: {subtraction:N2}");
+            Console.WriteLine($"The {nameof(division)} of the numbers is: {division:N}");
+            Console.WriteLine($"The {nameof(multiplication)} of the numbers is: {multiplication:N}");
+            Console.WriteLine($"The {nameof(addition)} of the numbers is: {addition:N}");
+            Console.WriteLine($"The {nameof(subtraction)} of the numbers is: {subtraction:N}");
+
+
+            Console.WriteLine("Testing int wrapping functionality");
+
+            var start = int.MaxValue - 4;
+
+            for (int i = 0; i < 10; i++)
+            {
+                //i =- 0, 1, 2, 3, 4, 5, 6, 7,8, 9, break
+                var output = start + i;
+                Console.WriteLine($"Current value: {output}");
+                if (i == 7)
+                {
+                    break;
+                }
+            }
+
+            var index = 0;
+            while (index < 10)
+            {
+                var output = start + index;
+                Console.WriteLine($"Current value: {output}");
+                if (index == 7)
+                {
+                    break;
+                }
+                index++;
+            }
+
         }
     }
 }
